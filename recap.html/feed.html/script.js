0f17620b97bc10bdb1d6bb6b97dd431812ed9cfb
MimiLKT <estrellalokitaa@gmail.com>
2025-02-20 18:39:30 +0100

    Update script.js

--- recap.html/feed.html/script.js
+++ recap.html/feed.html/script.js
@@ -1,3 +1,19 @@
+const starContainer = document.querySelector('.stars-background');
+
+// Crée 50 étoiles aléatoires
+for (let i = 0; i < 50; i++) {
+  const star = document.createElement('div');
+  star.classList.add('star');
+  
+  // Position et taille aléatoires
+  star.style.top = `${Math.random() * 100}%`;
+  star.style.left = `${Math.random() * 100}%`;
+  const size = Math.random() * 3 + 1; // Taille entre 1px et 4px
+  star.style.width = `${size}px`;
+  star.style.height = `${size}px`;
+
+  starContainer.appendChild(star);
+}
 document.addEventListener("DOMContentLoaded", () => {
     let projects = JSON.parse(localStorage.getItem("projects")) || [];
     const feedContainer = document.getElementById("feed");
